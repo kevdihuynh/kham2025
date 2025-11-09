c86093f277f9c10b811140f4dd0784c28990674b
kevdihuynh <kevdihuynh@users.noreply.github.com>
2025-11-09 10:55:08 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/wedding/',  // adjust if repo name differs
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  }
 });
 
